0d4b1826e5cefed38760c616664ba9f573a4a817
Peter Kubasov <kubasov@gmail.com>
2025-12-21 15:30:22 +0300

    upd css

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from "@astrojs/mdx";
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+	output: "static",
 	integrations: [mdx()],
 	markdown: {
 		shikiConfig: {
